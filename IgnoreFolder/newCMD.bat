73e0ef49a75b8d89281fdb19ef55b14a9da1d921
Alien663 <rite2844@gmail.com>
2023-01-09 09:27:35 +0800

    Delete IgnoreFolder directory

--- IgnoreFolder/newCMD.bat
+++
@@ -1,9 +0,0 @@
-::shutdown -r -t 10
-start cmd /c "ping tdshv02 -t"
-start cmd /c "ping tdshv04 -t"
-start cmd /c "ping tdshv05 -t"
-start cmd /c "ping tdshv07 -t"
-::start cmd /c "ping tdsap31 -t"
-::start cmd /c "ping tdsft02 -t"
-::start cmd /c "ping 10.243.29.115 -t"
-
