6af06c868d14fe29e0fc0f463fba168811852650
Alien663 <rite2844@gmail.com>
2023-06-02 11:39:35 +0800

    chore:Optimize Regedit Modify and Other Note

--- Powershell/Others.ps1
+++ Powershell/Others.ps1
@@ -8,3 +8,13 @@ $physicalPath = (Get-Website | Select-Object).PhysicalPath
 
 # declare object list
 $data = @( @{ Name="123";YearOld=15}, @{Name="ABC";YearOld=20})
+
+# object array can not modify
+$array1 = @("A", "B", "C")
+$array1.GetType()
+$array1.Add("D")
+
+# ArrayList array can modify
+$array2 = [System.Collections.ArrayList]@("A", "B", "C")
+$array2.GetType()
+$array2.Add("D")
--- Powershell/Remove3DESCBC.ps1
+++ Powershell/Remove3DESCBC.ps1
@@ -1,6 +1,10 @@
+$today = get-date -format "yyyy-MM-dd"
 $regedit_path = "HKLM:\SYSTEM\CurrentControlSet\Control\Cryptography\Configuration\Local\SSL\00010002"
-$prop = Get-ItemProperty -Path $regedit_path
-mkdir "D:\Remove3DESCBC"
-echo $prop.Functions >> "D:\Remove3DESCBC\log.txt"
-echo $prop.Functions -like "*3DES*"
-Set-ItemProperty -Path $regedit_path -Name "Functions" -Value ($prop.Functions -notlike "*3DES")
+$prop = (Get-ItemProperty -Path $regedit_path).Functions
+$folder = "D:\Remove3DESCBC"
+if(!(Test-Path $folder)){
+    New-Item $folder -Type Directory
+}
+echo $prop >> "D:\Remove3DESCBC\log_$today.txt"
+echo $prop -like "*3DES*"
+Set-ItemProperty -Path $regedit_path -Name "Functions" -Value ($prop -notlike "*3DES*")
